d069ce1f3bf6c5da78795cab2cf05e1f45e4b10d
J Fixby <github@jfixby.com>
2017-12-19 21:16:38 +0100

    upload

---
+++ android/src/com/jfixby/dcr/jet/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jfixby.dcr.jet;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jfixby.dcr.jet.JetEntryPoint;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JetEntryPoint(), config);
+	}
+}
