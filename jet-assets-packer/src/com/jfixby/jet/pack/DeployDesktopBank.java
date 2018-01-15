0d366f89274a7fadcdcdb262c6c092b1422b3b65
J Fixby <github@jfixby.com>
2018-01-15 14:33:02 +0100

    +base assets

--- jet-assets-packer/src/com/jfixby/jet/pack/DeployDesktopBank.java
+++ jet-assets-packer/src/com/jfixby/jet/pack/DeployDesktopBank.java
@@ -3,16 +3,35 @@ package com.jfixby.jet.pack;
 
 import java.io.IOException;
 
+import com.github.wrebecca.bleed.RebeccaTextureBleeder;
 import com.jfixby.jet.asset.JetLocalAssets;
+import com.jfixby.psd.unpacker.api.PSDUnpacker;
+import com.jfixby.psd.unpacker.core.RedPSDUnpacker;
 import com.jfixby.r3.assets.packer.go.SystemAssetsBankBuilder;
+import com.jfixby.scarabei.api.desktop.ImageAWT;
 import com.jfixby.scarabei.api.file.File;
 import com.jfixby.scarabei.api.file.LocalFileSystem;
+import com.jfixby.scarabei.api.names.ID;
+import com.jfixby.scarabei.api.names.Names;
 import com.jfixby.scarabei.red.desktop.ScarabeiDesktop;
+import com.jfixby.scarabei.red.desktop.image.RedImageAWT;
+import com.jfixby.texture.slicer.api.TextureSlicer;
+import com.jfixby.texture.slicer.red.RedTextureSlicer;
+import com.jfixby.tools.bleed.api.TextureBleed;
+import com.jfixby.tools.gdx.texturepacker.GdxTexturePacker;
+import com.jfixby.tools.gdx.texturepacker.api.TexturePacker;
 
 public class DeployDesktopBank {
 
 	public static void main (final String[] args) throws IOException {
 		ScarabeiDesktop.deploy();
+		PSDUnpacker.installComponent(new RedPSDUnpacker());
+		TexturePacker.installComponent(new GdxTexturePacker());
+		TextureSlicer.installComponent(new RedTextureSlicer());
+// TextureBleed.installComponent(new MaskTextureBleeder());
+		ImageAWT.installComponent(new RedImageAWT());
+		TextureBleed.installComponent(new RebeccaTextureBleeder());
+		// ---------------------------------------
 
 		final File localAssets = LocalFileSystem.ApplicationHome().parent().child("jet-desktop").child("assets");
 
@@ -21,8 +40,21 @@ public class DeployDesktopBank {
 		bankJet.clearFolder();
 		bankJet = SystemAssetsBankBuilder.writeBankHeader(localAssets, JetLocalAssets.LOCAL_BANK_NAME.toString());
 
-		final File jetPackages = bankJet.child("tank-0");
-		jetPackages.makeFolder();
+		final File tankFolder = bankJet.child("tank-0");
+		tankFolder.makeFolder();
+
+		final int fokkerStarterConfigWidth = 1024;
+		final int fokkerStarterConfigHeight = 800;
+		final String title = "Jet Wallet";
+		SystemAssetsBankBuilder.createStarterConfig(tankFolder, JetLocalAssets.FOKKER_STARTER_ASSET_ID, fokkerStarterConfigWidth,
+			fokkerStarterConfigHeight, title);
+
+		final File HOME = LocalFileSystem.ApplicationHome();
+		{
+			final File psdFile = HOME.parent().child("jet-assets").child("raw").child("patch-9.psd");
+			final ID package_name = Names.newID("com.jfixby.jet.patch-9");
+			Repack9PatchScene.rePack(tankFolder, psdFile, package_name);
+		}
 
 	}
 
--- jet-desktop/src/com/jfixby/jet/desktop/JetDesktopAssembler.java
+++ jet-desktop/src/com/jfixby/jet/desktop/JetDesktopAssembler.java
@@ -19,24 +19,14 @@ public class JetDesktopAssembler implements EngineAssembler {
 	@Override
 	public void assembleEngine () {
 		Screen.setDebugScaleFactor(1);
-
-// try {
-// testIndex();
-// } catch (final IOException e) {
-// e.printStackTrace();
-// }
-// Sys.exit();
-
 	}
 
 	private static void testIndex () throws IOException {
-
 		final File home = LocalFileSystem.ApplicationHome();
 		if (!home.getName().equalsIgnoreCase("assets")) {
 			L.d("Application home folder is not set to the android assets dir", home);
 			return;
 		}
-
 // final String java_path = "D:\\Tinto\\Tinto-gdx\\android\\assets\\";
 		final File androidAssets = home;
 		final File indexFile = androidAssets.child(GdxAssetsFileSystemIndex.INDEX_FILE_NAME);
